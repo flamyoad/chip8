855489b4578cd39f88bbe6e5c4b95d8b57977f54
flamyoad <yizake1820@gmail.com>
2022-06-12 16:21:04 +0800

    ehe

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,33 @@
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
  
 int main(int argc, char** argv) {
     SDL_Window* window = NULL;
     SDL_Surface* surface = NULL;
+
+    bool quit = false;
+
+    // Event handler
+    SDL_Event e;
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError();
@@ -39,4 +62,11 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+bool init() {
+    // TODO: continue from here 
+    // http://lazyfoo.net/tutorials/SDL/02_getting_an_image_on_the_screen/index.php
+}
+
+
 
